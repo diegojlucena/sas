448ceff84d4edd01cb0c398ad80e1109378397d6
Diego Lucena <diegojlucena@gmail.com>
2011-02-22 11:57:59 -0300

    modificando assets controller para trabalhar com respond_with

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,83 +1,42 @@
 class AssetsController < ApplicationController
-  # GET /assets
-  # GET /assets.xml
+  respond_to :html
+  
   def index
     @assets = Asset.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @assets }
-    end
+    respond_with(@assets)
   end
 
-  # GET /assets/1
-  # GET /assets/1.xml
   def show
     @asset = Asset.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @asset }
-    end
+    respond_with(@asset)
   end
 
-  # GET /assets/new
-  # GET /assets/new.xml
   def new
     @asset = Asset.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @asset }
-    end
+    respond_with(@asset)
   end
 
-  # GET /assets/1/edit
   def edit
     @asset = Asset.find(params[:id])
+    respond_with(@asset)
   end
 
-  # POST /assets
-  # POST /assets.xml
   def create
     @asset = Asset.new(params[:asset])
-
-    respond_to do |format|
-      if @asset.save
-        format.html { redirect_to(@asset, :notice => 'Asset was successfully created.') }
-        format.xml  { render :xml => @asset, :status => :created, :location => @asset }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @asset.errors, :status => :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Ativo criado com sucesso.' if @asset.save
+    respond_with(@asset)
   end
 
-  # PUT /assets/1
-  # PUT /assets/1.xml
   def update
     @asset = Asset.find(params[:id])
-
-    respond_to do |format|
-      if @asset.update_attributes(params[:asset])
-        format.html { redirect_to(@asset, :notice => 'Asset was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @asset.errors, :status => :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Ativo atualizado com sucesso.' if @asset.update_attributes(params[:asset])
+    respond_with(@asset)
   end
 
-  # DELETE /assets/1
-  # DELETE /assets/1.xml
   def destroy
     @asset = Asset.find(params[:id])
     @asset.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(assets_url) }
-      format.xml  { head :ok }
-    end
+    flash[:notice] = "Ativo apagado com sucesso."  
+    respond_with(@asset)
   end
 end
